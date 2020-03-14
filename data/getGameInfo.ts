ad4afa6b7efcf97c56a1ef54b645ecb49407d56e
Ben Joldersma <ben@maven.io>
2020-03-13 23:46:50 -0700

    add connections

--- data/getGameInfo.ts
+++ data/getGameInfo.ts
@@ -1,6 +1,14 @@
 import Airtable from 'airtable'
 var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appI5M2FFteHsHsl7');
 
+export enum Test {
+  Climb,
+  Search,
+  'Pick Lock',
+  Charm,
+  Intimidate
+}
+
 // maybe useful?
 export interface Place {
     Name: string
@@ -11,7 +19,16 @@ export interface Place {
     Enemy?: string[],
 }
 
+export interface Connection {
+    Name: string,
+    From: string[],
+    To: string[],
+    Description: string
+    Test: Test
+}
+
 const places = base('Places')
+const connections = base('Connections')
 
 export const getPlaces = async () => {
     let ret: Place[] = []
@@ -24,3 +41,13 @@ export const getPlaces = async () => {
     return ret
 }
 
+export const getConnections = async () => {
+    let ret: Connection[] = []
+    await connections.select({
+        view: 'Grid view'
+    }).eachPage((records, fetchNextPage) => {
+        ret = [...ret, ...records.map(r => r.fields as Connection)]
+        fetchNextPage()
+    })
+    return ret
+}
--- pages/api/gameInfo.ts
+++ pages/api/gameInfo.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import {getPlaces} from '../../data/getGameInfo'
+import {getConnections, getPlaces} from '../../data/getGameInfo'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const result = {
-    places: await getPlaces()
+    places: await getPlaces(),
+    connections: await getConnections()
   }
   res.status(200).json(result)
 }
