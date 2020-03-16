d98853bcaef3556632e794464bd82ece168016a2
Ben Joldersma <ben@maven.io>
2020-03-15 22:47:14 -0700

    add caching, enemy slurp

--- data/getGameInfo.ts
+++ data/getGameInfo.ts
@@ -1,4 +1,6 @@
 import Airtable from 'airtable'
+import cache from 'memory-cache'
+
 var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appI5M2FFteHsHsl7');
 
 export enum Test {
@@ -29,27 +31,49 @@ export interface Connection {
     Test: Test
 }
 
-const places = base('Places')
-const connections = base('Connections')
+export interface Enemy {
+    Id: string
+    Name: string
+    'Alive Description': string
+    'Dead Description': string
+    'Dying Scene'?: string
+    Picture?: any
+    WS: number
+    RS: number
+    MS: number
+    Health: number
+    Damage: number
+    Random: boolean
+    Level: number
+}
+
+const cacheTtlInMS = 30000
 
 export const getPlaces = async () => {
-    let ret: Place[] = []
-    await places.select({
-        view: 'Grid view'
-    }).eachPage((records, fetchNextPage) => {
-        ret = [...ret, ...records.map(r => ({ ...r.fields, Id: r.id } as Place))]
-        fetchNextPage()
-    })
-    return ret
+    return getTable<Place>('Places')
 }
 
 export const getConnections = async () => {
-    let ret: Connection[] = []
-    await connections.select({
+    return getTable<Connection>('Connections')
+}
+
+export const getEnemies = async () => {
+    return getTable<Enemy>('Enemies')
+}
+
+const getTable = async <T>(tableName: string) => {
+    let ret: T[] = []
+    var cacheKey = tableName
+    const table = base(tableName)
+    if (cache.get(cacheKey)) {
+        return cache.get(cacheKey)
+    }
+    await table.select({
         view: 'Grid view'
     }).eachPage((records, fetchNextPage) => {
-        ret = [...ret, ...records.map(r => ({ ...r.fields, Id: r.id } as Connection))]
+        ret = [...ret, ...records.map(r => ({...r.fields, Id: r.id} as unknown as T))]
         fetchNextPage()
     })
+    cache.put(cacheKey, ret, cacheTtlInMS)
     return ret
 }
